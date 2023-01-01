b054b0cfe626298f5226c05790877d3668a679d2
murat <slipstream11@yandex.com>
2023-01-01 13:58:37 +0300

    else if commit

---
+++ js/015-if-else.js
@@ -0,0 +1,38 @@
+//**** Koşullarla Çalışmak if - else */
+
+let username = prompt("kullanıcı adı giriniz : ")
+//Eğer Kullanıcı Bilgisi varsa ekrana yazdır
+// eğer (username.length > 0) {console.log(username)} değilse{console.log("bilgiyok")}
+// if (username.length > 0) {console.log(username)} else {console.log("bilgi yok")}
+
+if (username) {    // if kısmı true ise çalışır
+    console.log(`Kullanıcı Bilginiz ${username}`)
+} else {
+    console.log("bilgi yok")
+}
+// `` baacktick!!!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.body.style.backgroundColor="darkgray"
---
+++ js/016-coklu-kosullar.js
@@ -0,0 +1,43 @@
+// **** Coklu Kosullarla Çalışmak else if
+let username =  prompt("kullanıcı adınız:")
+let age =  prompt ("Yasınız")
+let info = document.querySelector("#info")
+
+// if (username && age >= 18) {
+//     console.log("ehliyet alabilir")
+// } else if (!username) {
+//     console.log("kullanıcı adınız yok")
+// } else if (!(age >= 18)) {
+//     console.log("yaş bilgisi yok veya 18den ufak")
+// }
+
+// if (username && age >= 18) {
+//     info.innerHTML = "ehliyet alabilir"
+// } else if (!username) {
+//     info.innerHTML = "kullanıcı adınız yok"
+// } else if (!(age >= 18)) {
+//     info.innerHTML = "yaş bilgisi yok veya 18den ufak"
+// }
+
+// if (username && age >= 18 ) {console.log("passed")
+// } else if (!username) {console.log("isim gir")
+// } else if (!(age >= 18)) {console.log("yaş gir")}
+
+if (username && age >= 18 ) { info.innerHTML = ("passed")
+} else if (!username) {info.innerHTML = ("isim gir")
+} else if (!(age >= 18)) {info.innerHTML = ("yaş gir")}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.body.style.backgroundColor="darkgray"
