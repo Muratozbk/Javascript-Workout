cfb0bace4f9faa3ecdcb830f263560249e998465
murat <slipstream11@yandex.com>
2023-01-01 20:27:13 +0300

    repeat commit

--- js/denemeler.js
+++ js/denemeler.js
@@ -34,29 +34,38 @@
 // liDOM2.innerHTML = "prependli öge"
 // ulDOM.prepend(liDOM2)
 
-let lastChild = document.querySelector("ul#list>li:last-child")
-lastChild.innerHTML = "son öge değiş"
-console.log(lastChild)
+// let lastChild = document.querySelector("ul#list>li:last-child")
+// lastChild.innerHTML = "son öge değiş"
+// console.log(lastChild)
 
-let firstChild = document.querySelector("ul#list>li:first-child")
-firstChild.innerHTML = "ilk öge degis"
-console.log(firstChild)
+// let firstChild = document.querySelector("ul#list>li:first-child")
+// firstChild.innerHTML = "ilk öge degis"
+// console.log(firstChild)
 
-let ulDOM = document.querySelector("ul#list")
-let liDOM = document.createElement("li")
-liDOM.innerHTML = "yeni satır 1 append"
-ulDOM.append(liDOM)
+// let ulDOM = document.querySelector("ul#list")
+// let liDOM = document.createElement("li")
+// liDOM.innerHTML = "yeni satır 1 append"
+// ulDOM.append(liDOM)
 
-let liDOM2 = document.createElement("li")
-liDOM2.innerHTML = "yeni 2 prepend"
-ulDOM.prepend(liDOM2)
+// let liDOM2 = document.createElement("li")
+// liDOM2.innerHTML = "yeni 2 prepend"
+// ulDOM.prepend(liDOM2)
 
+// let username = prompt("kullanıcı adınız")
+// if (username) {console.log(`Kullanıcı adı: ${username}`)} else {console.log("boş")}
+// let age = prompt("yaş gir")
+// let info = document.querySelector("#info")
 
+// if (username && age >= 18) {info.innerHTML = ("geçtiniz")} else if (!(username)) {info.innerHTML = ("isim yaz")}
+// else if (!(age >=18)) {info.innerHTML = ("sıçtınız")}
 
+// if (username && age >= 18) {info.innerHTML =("geçti")} else if (!username) {
+//     info.innerHTML = ("isim yaz")
+// } else if (!(age >= 18)){info.innerHTML = ("kremyap")}
 
-
-
-
+let username = prompt("Kullanıcı Adı Gir")
+let info =document.querySelector("#info")
+info.innerHTML = username ? username: "isim yaz"
 
 
 
